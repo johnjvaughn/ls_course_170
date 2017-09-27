c2d4c82b86fd571f566496014b509939dfbd1703
John Vaughn <johnjvaughn@gmail.com>
2017-09-26 22:13:44 -0400

    Sinatra Todos cont.

--- sinatra_todos/todo.rb
+++ sinatra_todos/todo.rb
@@ -30,6 +30,20 @@ helpers do
   def num_todos(list)
     list[:todos].size
   end
+
+  def sort_lists(lists, &block)
+    complete_lists, incomplete_lists = 
+      lists.partition { |list| list_complete?(list) }
+    incomplete_lists.each { |list| yield list, lists.index(list)}
+    complete_lists.each { |list| yield list, lists.index(list)}
+  end
+
+  def sort_todos(todos, &block)
+    complete_todos, incomplete_todos = 
+      todos.partition { |todo| todo[:completed] }
+    incomplete_todos.each { |todo| yield todo, todos.index(todo)}
+    complete_todos.each { |todo| yield todo, todos.index(todo)}
+  end
 end
 
 before do
